1dd7235d957d4980f5ce6ca37763d44c68bd89ae
austinbhan <austinbhan@outlook.com>
2022-09-15 16:11:19 -0700

    get id and test

--- __tests__/simple-db.test.js
+++ __tests__/simple-db.test.js
@@ -1,5 +1,7 @@
 const fs = require('fs/promises');
 const path = require('path');
+const crypto = require('crypto');
+const SimpleDb = require('../lib/simple-db');
 
 const { CI, HOME } = process.env;
 const BASE_DIR = CI ? HOME : __dirname;
@@ -12,8 +14,16 @@ describe('simple database', () => {
     await fs.mkdir(TEST_DIR, { recursive: true });
   });
 
-  it('needs a first test...', async () => {
-
+  it('get(id) returns file contents', async () => {
+    const obj = {
+      name: '1234'
+    };
+    const id = crypto.randomBytes(4).toString('hex');
+    await fs.writeFile(`${TEST_DIR}/${id}.json`, JSON.stringify(obj));
+  
+    const db = new SimpleDb(TEST_DIR);
+    const result = await db.get(id);
+    expect(result).toEqual(obj);
   });
 
 });
